912994143e20ef19aa168b44268c28163596bd3e
Alov Maxim <alovmax@yandex.ru>
2013-09-14 20:44:59 +0400

    Added sqlite driver sources.

---
+++ sqlite3/sqlite_driver.cc
@@ -0,0 +1,10 @@
+/*
+ * sqlite_driver.cc
+ *
+ *  Description: Driver for SQLite 3 implementation.
+ *
+ *  Created on: 14.09.2013
+ *      Author: Maxim Alov <m.alov@samsung.com>
+ */
+
+#include "sqlite_driver.h"
---
+++ sqlite3/sqlite_driver.h
@@ -0,0 +1,17 @@
+/*
+ * sqlite_driver.h
+ *
+ *  Description: Driver for SQLite 3 declaration.
+ *
+ *  Created on: 14.09.2013
+ *      Author: Maxim Alov <m.alov@samsung.com>
+ */
+
+#ifndef SQLITE_DRIVER_H_
+#define SQLITE_DRIVER_H_
+
+#include "sqlite3.h"
+
+
+
+#endif /* SQLITE_DRIVER_H_ */
