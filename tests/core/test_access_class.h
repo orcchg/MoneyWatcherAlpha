7df2cbd10213223b3d3bbe172282df82986a3b61
Alov Maxim <alovmax@yandex.ru>
2013-09-28 13:05:14 +0400

    [ADD] Added TestAccess class header.

---
+++ tests/core/test_access_class.h
@@ -0,0 +1,22 @@
+/*
+ * test_access_class.h
+ *
+ *  Description: Test class to access private fields of core items, declaration.
+ *
+ *  Created on: 28.09.2013
+ *      Author: Maxim Alov <m.alov@samsung.com>
+ */
+
+#ifndef TEST_ACCESS_CLASS_H_
+#define TEST_ACCESS_CLASS_H_
+
+
+namespace mw {
+
+class TestAccess {
+public:
+};
+
+}  /* namespace mw */
+
+#endif /* TEST_ACCESS_CLASS_H_ */
