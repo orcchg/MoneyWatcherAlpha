6ea7aae04d66195a02ffbb67de2041e90c5efbb2
Alov Maxim <alovmax@yandex.ru>
2013-09-30 23:10:14 +0400

    [MODIF] Updated addRecord unit-test

--- tests/core/test_access_class.h
+++ tests/core/test_access_class.h
@@ -10,11 +10,26 @@
 #ifndef TEST_ACCESS_CLASS_H_
 #define TEST_ACCESS_CLASS_H_
 
+#include <string>
+#include "daily_table.h"
+#include "types.h"
+
 
 namespace mw {
 
 class TestAccess {
 public:
+  TestAccess(DailyTable* ptr_daily_table);
+  virtual ~TestAccess();
+
+  const DB_Handler& getDbHandler() const;
+  ID_t getNextID() const;
+  const std::string& getTableName() const;
+
+private:
+  DailyTable* m_ptr_daily_table;
+  ID_t m_next_id;
+  std::string m_table_name;
 };
 
 }  /* namespace mw */
--- tests/core/test_all.cc
+++ tests/core/test_all.cc
@@ -12,6 +12,7 @@
 #include <thread>
 #include <unistd.h>
 #include "tests/google/gtest/gtest.h"
+#include "test_access_class.h"
 #include "test_fixture.h"
 #include "test_include_all.h"
 
@@ -42,15 +43,66 @@ TEST (DailyTableTest, CreateDailyTable) {
 
 TEST (DailyTableTest, AddRecord) {
   mw::DailyTable daily_table("Test-DailyTable.db");
-  MoneyValue_t balance = 1000;
-  std::wstring description = L"Тестовая запись в таблице";
-  mw::Status status(mw::SV_INCOME);
-  mw::Record record = daily_table.addRecord(balance, description, status);
+  mw::TestAccess accessor(&daily_table);
+  MoneyValue_t s_balance = 1000;
+  std::wstring s_description = L"Тестовая запись в таблице";
+  mw::Status s_status(mw::SV_INCOME);
+  mw::Record record = daily_table.addRecord(s_balance, s_description, s_status);
+  EXPECT_EQ(record.getID(), accessor.getNextID());  // TODO: check validity of next_id
+  EXPECT_EQ(record.getBalance(), s_balance);
+  EXPECT_STREQ(record.getDescription().c_str(), s_description.c_str());
+  EXPECT_EQ(record.getStatus(), s_status);
+
+  std::string count_statement = "SELECT COUNT(*) FROM \'";
+  count_statement += accessor.getTableName();
+  count_statement += "\'";
+  int nByte = static_cast<int>(count_statement.length());
+  DB_Statement statement_handler = nullptr;
+  int result = sqlite3_prepare_v2(
+      accessor.getDbHandler(),
+      count_statement.c_str(),
+      nByte,
+      &statement_handler,
+      nullptr);
+  int is_row = sqlite3_step(statement_handler);
+  ASSERT_EQ(result, SQLITE_OK);
+  ASSERT_EQ(is_row, SQLITE_ROW);
+  is_row = sqlite3_step(statement_handler);
+  ASSERT_EQ(is_row, SQLITE_DONE);
+  sqlite3_finalize(statement_handler);
+
+  std::string value_statement = "SELECT * FROM \'";
+  value_statement += accessor.getTableName();
+  value_statement += "\'";
+  nByte = static_cast<int>(value_statement.length());
+  result = SQLITE_ERROR;
+  result = sqlite3_prepare_v2(
+      accessor.getDbHandler(),
+      value_statement.c_str(),
+      nByte,
+      &statement_handler,
+      nullptr);
+  is_row = SQLITE_ERROR;
+  is_row = sqlite3_step(statement_handler);
+  ASSERT_EQ(result, SQLITE_OK);
+  ASSERT_EQ(is_row, SQLITE_ROW);
+  ID_t id = sqlite3_column_int64(statement_handler, 0);
+  EXPECT_EQ(record.getID(), id);
+  std::string date(reinterpret_cast<const char*>(sqlite3_column_text(statement_handler, 1)));
+  std::string time(reinterpret_cast<const char*>(sqlite3_column_text(statement_handler, 2)));
+  mw::DateTime datetime(date, time);
+  EXPECT_EQ(record.getDateTime(), datetime);  // TODO: invalid datetime
+  MoneyValue_t balance = sqlite3_column_int64(statement_handler, 3);
   EXPECT_EQ(record.getBalance(), balance);
-  EXPECT_STREQ(record.getDescription().c_str(), description.c_str());
+  const void* raw_description = sqlite3_column_text16(statement_handler, 4);
+  std::wstring description(static_cast<const wchar_t*>(raw_description));
+  EXPECT_STREQ(record.getDescription().c_str(), description.c_str());  // TODO: written only 1st word
+  sqlite3_int64 raw_status = sqlite3_column_int64(statement_handler, 5);
+  mw::Status status(raw_status);
   EXPECT_EQ(record.getStatus(), status);
-  // TODO: SQL-query to count rows in table
-  // TODO: SQL-query to get column values and compare with actual in memory
+  is_row = sqlite3_step(statement_handler);
+  ASSERT_EQ(is_row, SQLITE_DONE);
+  sqlite3_finalize(statement_handler);
 }
 
 TEST (DailyTableTest, ReadRecord) {
