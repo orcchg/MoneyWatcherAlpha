7b7761a9c8e6c62db9ebe22bb09cce0957e70a7d
Alov Maxim <alovmax@yandex.ru>
2013-10-02 20:36:18 +0400

    [MODIF] Added log level from MW_LOG_LEVEL env var, set during configure

--- include/logger.h
+++ include/logger.h
@@ -48,12 +48,40 @@
 #define DBG_SUGGEST COLOR_OPEN DBG_COLOR DBG_STRING __FILE__ COLON LINE PROMPT_SUGGEST
 #define TRC_SUGGEST COLOR_OPEN TRC_COLOR TRC_STRING __FILE__ COLON LINE PROMPT_SUGGEST
 
+#define CRT(fmt, ...)
+#define ERR(fmt, ...)
+#define WRN(fmt, ...)
+#define INF(fmt, ...)
+#define DBG(fmt, ...)
+#define TRC(fmt, ...)
+
+#define LOG_LEVEL __MW_LOG_LEVEL__
+
+#if (LOG_LEVEL >= 0)
+#undef CRT
 #define CRT(fmt, ...) printf((CRT_SUGGEST #fmt PROMPT_CLOSE), __LINE__, ##__VA_ARGS__)
+#else
+#endif
+#if (LOG_LEVEL >= 1)
+#undef ERR
 #define ERR(fmt, ...) printf((ERR_SUGGEST #fmt PROMPT_CLOSE), __LINE__, ##__VA_ARGS__)
+#endif
+#if (LOG_LEVEL >= 2)
+#undef WRN
 #define WRN(fmt, ...) printf((WRN_SUGGEST #fmt PROMPT_CLOSE), __LINE__, ##__VA_ARGS__)
+#endif
+#if (LOG_LEVEL >= 3)
+#undef INF
 #define INF(fmt, ...) printf((INF_SUGGEST #fmt PROMPT_CLOSE), __LINE__, ##__VA_ARGS__)
+#endif
+#if (LOG_LEVEL >= 4)
+#undef DBG
 #define DBG(fmt, ...) printf((DBG_SUGGEST #fmt PROMPT_CLOSE), __LINE__, ##__VA_ARGS__)
+#endif
+#if (LOG_LEVEL >= 5)
+#undef TRC
 #define TRC(fmt, ...) printf((TRC_SUGGEST #fmt PROMPT_CLOSE), __LINE__, ##__VA_ARGS__)
+#endif
 
 #else
 
