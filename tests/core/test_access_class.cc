72913318c5b83d8f13f8ba3a7d6dcb60cbc3c5a1
Alov Maxim <alovmax@yandex.ru>
2013-10-06 21:50:02 +0400

    [MODIF] Modified TestAccess class, eliminated code duplication.

--- tests/core/test_access_class.cc
+++ tests/core/test_access_class.cc
@@ -12,27 +12,4 @@
 
 namespace mw {
 
-TestAccess::TestAccess(DailyTable* i_ptr_daily_table)
-  : m_ptr_daily_table(i_ptr_daily_table) {
-}
-
-TestAccess::~TestAccess() {
-}
-
-const DB_Handler& TestAccess::getDbHandler() const {
-  return (this->m_ptr_daily_table->m_db_handler);
-}
-
-ID_t TestAccess::getNextID() const {
-  return (this->m_ptr_daily_table->m_next_record_id);
-}
-
-const std::string& TestAccess::getTableName() const {
-  return (this->m_ptr_daily_table->m_table_name);
-}
-
-bool TestAccess::checkFinalized() const {
-  return (!this->m_ptr_daily_table->m_db_statement);
-}
-
 }  /* namespace mw */
--- tests/core/test_access_class.h
+++ tests/core/test_access_class.h
@@ -17,10 +17,11 @@
 
 namespace mw {
 
-class TestAccess {
+template <typename Table>
+class TestAccessTable {
 public:
-  TestAccess(DailyTable* ptr_daily_table);
-  virtual ~TestAccess();
+  TestAccessTable(Table* ptr_daily_table);
+  virtual ~TestAccessTable();
 
   const DB_Handler& getDbHandler() const;
   ID_t getNextID() const;
@@ -29,8 +30,38 @@ public:
   bool checkFinalized() const;
 
 private:
-  DailyTable* m_ptr_daily_table;
+  Table* m_ptr_table;
 };
+
+
+template <typename Table>
+TestAccessTable<Table>::TestAccessTable(Table* i_ptr_table)
+  : m_ptr_table(i_ptr_table) {
+}
+
+template <typename Table>
+TestAccessTable<Table>::~TestAccessTable() {
+}
+
+template <typename Table>
+const DB_Handler& TestAccessTable<Table>::getDbHandler() const {
+  return (this->m_ptr_table->m_db_handler);
+}
+
+template <typename Table>
+ID_t TestAccessTable<Table>::getNextID() const {
+  return (this->m_ptr_table->m_next_id);
+}
+
+template <typename Table>
+const std::string& TestAccessTable<Table>::getTableName() const {
+  return (this->m_ptr_table->m_table_name);
+}
+
+template <typename Table>
+bool TestAccessTable<Table>::checkFinalized() const {
+  return (!this->m_ptr_table->m_db_statement);
+}
 
 }  /* namespace mw */
 
