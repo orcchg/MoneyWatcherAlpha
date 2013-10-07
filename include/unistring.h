4e76678baffb71fdd6e2db62ddff04fb072d404f
Alov Maxim <alovmax@yandex.ru>
2013-10-08 00:55:56 +0400

    [ADD] Added unistring wrapper declaration.

---
+++ include/unistring.h
@@ -0,0 +1,18 @@
+/*
+ * unistring.h
+ *
+ *  Description: String wrapper for various locales declaration.
+ *
+ *  Created on: 08.10.2013
+ *      Author: Maxim Alov <m.alov@samsung.com>
+ */
+
+#ifndef UNISTRING_H_
+#define UNISTRING_H_
+
+
+namespace mw {
+
+}  /* namespace mw */
+
+#endif /* UNISTRING_H_ */
---
+++ src/unistring.cc
@@ -0,0 +1,15 @@
+/*
+ * unistring.cc
+ *
+ *  Description: String wrapper for various locales implementation.
+ *
+ *  Created on: 08.10.2013
+ *      Author: Maxim Alov <m.alov@samsung.com>
+ */
+
+#include "unistring.h"
+
+
+namespace mw {
+
+}  /* namespace mw */
