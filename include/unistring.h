3aad7d184e8eb31c21ecab76803d40c4822c1d07
Alov Maxim <alovmax@yandex.ru>
2013-10-08 22:27:41 +0400

    [ADD] Implemented WrappedString wrapper class.

--- include/unistring.h
+++ include/unistring.h
@@ -10,8 +10,56 @@
 #ifndef UNISTRING_H_
 #define UNISTRING_H_
 
+#include <exception>
+#include <string>
+
+#define __UNISTRING_FORMAT_SPECIFIER__ "%ls"
+
 
 namespace mw {
+
+class WrappedString {
+  typedef std::wstring::value_type Char_t;
+  typedef std::wstring String_t;
+  WrappedString(const String_t& str);
+
+public:
+  WrappedString();
+  WrappedString(const void* raw_char_data);
+  virtual ~WrappedString();
+
+  size_t length() const;
+  int n_bytes() const;
+  const Char_t* c_str() const;
+
+  WrappedString& operator += (const WrappedString& rhs);
+
+  static WrappedString to_string (int val);
+  static WrappedString to_string (long val);
+  static WrappedString to_string (long long val);
+  static WrappedString to_string (unsigned val);
+  static WrappedString to_string (unsigned long val);
+  static WrappedString to_string (unsigned long long val);
+  static WrappedString to_string (float val);
+  static WrappedString to_string (double val);
+  static WrappedString to_string (long double val);
+
+private:
+  String_t m_string;
+};
+
+/// @class WrappedStringException
+/// @brief Represents a common exception raised by WrappedString class methods.
+class WrappedStringException : public std::exception {
+public:
+  WrappedStringException(const char* message);
+  virtual ~WrappedStringException() throw();
+
+  const char* what() const throw();
+
+private:
+  const char* m_message;
+};
 
 }  /* namespace mw */
 
--- src/unistring.cc
+++ src/unistring.cc
@@ -9,7 +9,96 @@
 
 #include "unistring.h"
 
+#define __TO_STRING__(val) (std::to_wstring(val))
+
 
 namespace mw {
+
+WrappedString::WrappedString(const String_t& str)  // private
+  : m_string(str) {
+}
+
+WrappedString::WrappedString() {
+}
+
+WrappedString::WrappedString(const void* raw_char_data)
+  : m_string(static_cast<const Char_t*>(raw_char_data)) {
+}
+
+WrappedString::~WrappedString() {
+}
+
+
+size_t WrappedString::length() const {
+  return (this->m_string.length());
+}
+
+int WrappedString::n_bytes() const {
+  return (static_cast<int>(this->m_string.length()) * sizeof(Char_t));
+}
+
+const WrappedString::Char_t* WrappedString::c_str() const {
+  return (this->m_string.c_str());
+}
+
+
+WrappedString& WrappedString::operator += (const WrappedString& rhs) {
+  this->m_string += rhs.m_string;
+  return (*this);
+}
+
+
+/* To string */
+// ----------------------------------------------
+WrappedString WrappedString::to_string (int val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (long val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (long long val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (unsigned val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (unsigned long val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (unsigned long long val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (float val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (double val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+WrappedString WrappedString::to_string (long double val) {
+  return (WrappedString(__TO_STRING__(val)));
+}
+
+
+
+/* WrappedString exception */
+// ----------------------------------------------------------------------------
+WrappedStringException::WrappedStringException(const char* i_message)
+  : m_message(i_message) {
+}
+
+WrappedStringException::~WrappedStringException() throw() {
+}
+
+const char* WrappedStringException::what() const throw() {
+  return (this->m_message);
+}
 
 }  /* namespace mw */
